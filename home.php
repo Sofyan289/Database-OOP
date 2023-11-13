7c1460b31fbe59203f9a148a9a86ba454ad31a2b
Sofyan289 <114239253+Sofyan289@users.noreply.github.com>
2023-11-13 15:03:19 +0100

    send

---
+++ home.php
@@ -0,0 +1,16 @@
+<?php
+include 'db.php';
+$db = new Database();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <H1>Text</H1>
+</body>
+</html>
