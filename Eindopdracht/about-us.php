2e0011b0a31aa0f6c74820738f7d4637970b5d74
Prawishay <prawishayjainath@gmail.com>
2024-01-12 10:30:08 +0100

    c

---
+++ Eindopdracht/about-us.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us - Carrental-24/7</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+
+    <header class="header-divider">
+        <h1>Carrental-<span>24/7</span></h1>
+        <div class="menu-toggle" onclick="toggleMenu()">&#9776;</div>
+    </header>
+
+    <div class="sidebar" id="sidebar">
+        <div class="close-btn" onclick="closeMenu()"><span>Close</span></div>
+        <a href="index.html" class="nav-link">Home</a>
+        <a href="" class="nav-link">Cars</a>
+        <a href="about.html" class="nav-link">About Us</a>
+        <a href="" class="nav-link">Sign-in</a>
+        <a href="" class="nav-link">Log-out</a>
+    </div>
+
+    <div class="content">
+        <h2>About Us</h2>
+        <p>Carrental-24/7 is more than a car rental service; we're your partners in exploration. Our mission is to provide you with the keys to unforgettable journeys, offering top-notch vehicles and a commitment to exceptional service. Let's embark on adventures together!</p>
+    </div>
+
+    <script src="script.js"></script>
+</body>
+
+</html>
--- Eindopdracht/homepagina.php
+++ Eindopdracht/homepagina.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Autoverhuur24</title>
+    <title>Carrental-24/7</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 
@@ -17,11 +17,11 @@
 
     <div class="sidebar" id="sidebar">
         <div class="close-btn" onclick="closeMenu()"><span>Close</span></div>
-        <a href="#" class="nav-link">Home</a>
-        <a href="#" class="nav-link">Cars</a>
-        <a href="#" class="nav-link">About-us</a>
-        <a href="#" class="nav-link">Sign-in</a>
-        <a href="#" class="nav-link">Log-out</a>
+        <a href="" class="nav-link">Home</a>
+        <a href="" class="nav-link">Cars</a>
+        <a href="about-us.php" class="nav-link">About-us</a>
+        <a href="" class="nav-link">Sign-in</a>
+        <a href="" class="nav-link">Log-out</a>
     </div>
 
     <div class="content">
@@ -47,7 +47,7 @@
             carouselIndex = (carouselIndex % 3) + 1; // Wissel tussen 1, 2 en 3
         }
 
-        setInterval(updateCarousel, 5000); // Wijzig elke 5 seconden
+        setInterval(updateCarousel, 5000); 
     </script>
 </body>
 
